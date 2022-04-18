7c4207f0910632b1e21bec952e2a946793e1bc18
Marcelle Monteiro <monteiro.bio@outlook.com>
2022-04-18 11:23:28 -0300

    feat: component timer insert

--- src/page/TimerInsert/index.js
+++ src/page/TimerInsert/index.js
@@ -1,15 +1,52 @@
 import React, { useState } from 'react';
 import MyTimer from '../Timer';
-import { Container } from './styles';
+import { Container, InputNumber } from './styles';
 
 const TimerInsert = () => {
-  const [timer, setTimer] = useState(false);
+  const [haveTimer, setHaveTimer] = useState(false);
+  const [time, setTime] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [second, setSecond] = useState(0);
+
+  const handleSetTimer = () => {
     const time = new Date();
     time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
+    setHaveTimer(true);
+
+  };
     return (
-      <div>
-      { timer && <MyTimer expiryTimestamp={time} /> }
-      </div>
+      <Container>
+        {
+        haveTimer
+        ? <MyTimer expiryTimestamp={time} />
+      : (
+      <form>
+          <label htmlFor="setHour">
+            <InputNumber 
+              type="number" name="hour" id="hour"
+              onChange={ ({target}) => setHour(target.value) }
+              value={ hour }
+              min="0"
+          />
+          </label>
+          <label htmlFor="setMinute">
+              <InputNumber type="number" name="minute" id="hour"
+              onChange={ ({target}) => setMinute(target.value) }
+              value={ minute }
+              min="0"
+          />
+          </label>
+          <label htmlFor="setSecond">
+              <InputNumber type="number" name="second" id="hour"
+              onChange={ ({target}) => setSecond(target.value) }
+              value={ second }
+              min="0"
+            />
+          </label>
+      </form>
+      )}
+    </Container>
     );
 }
 
