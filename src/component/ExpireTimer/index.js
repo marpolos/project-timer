6c39d1195740d19b087823b7a0e0e55776bc3973
Marcelle Monteiro <monteiro.bio@outlook.com>
2022-04-18 16:59:44 -0300

    fixed: audio

--- src/component/ExpireTimer/index.js
+++ src/component/ExpireTimer/index.js
@@ -4,11 +4,14 @@ import { Container, imageEnd } from './styles';
 const ExpireTimer = () => {
   return (
     <Container>
-      {/* <h2> ExpireTimer </h2> */}
       <imageEnd>
         <iframe title="expire timer" allow="fullscreen" frameBorder="0" height="361" src="https://giphy.com/embed/ocrF5ysjq1cs9VthAi/video" width="480">
         </iframe>
       </imageEnd>
+      <audio controls autoplay>
+      <source src="" type="audio/ogg" />
+        Your browser does not support the audio element.
+      </audio>
     </Container>
   );
 }
